7f5af8e48210afbb4582679bb1d5b45f066e2c11
Edvan Lima <edvan.lima@tripletech.com.br>
2024-06-04 22:16:02 -0300

    Initial commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,6 @@
+import { Slot } from "expo-router"
+import "@/styles/globals.css"
+
+export default function _layout() {
+  return <Slot />
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native"
+import React from "react"
+
+export default function Home() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl text-red-500">home</Text>
+    </View>
+  )
+}
